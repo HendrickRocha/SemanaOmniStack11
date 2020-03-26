6ad3b916fc1e064e61da1e986d7bd826d2976a8e
Hendrick Garcia da Rocha <hendrick.rocha2133@gmail.com>
2020-03-25 22:15:52 -0300

    Back-end

---
+++ backend/src/database/migrations/20200325190301_create_ongs.js
@@ -0,0 +1,15 @@
+/**Método Up é responsavel pela criação das tabelas */
+exports.up = function(knex) {
+  return knex.schema.createTable('ongs', function(table){
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
+  });
+};
+/**Down para dropar uma tabela */
+exports.down = function(knex) {
+ return knex.schema.droptable('ongs'); 
+};
